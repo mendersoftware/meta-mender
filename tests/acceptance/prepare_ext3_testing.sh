3c98f5e14bdc241daef87f5eb9d4cffdbe46076f
Kristian Amlie <kristian.amlie@mender.io>
2016-07-18 11:13:57 +0200

    Pick up secret credentials from common location instead of each workspace.
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>

--- tests/acceptance/prepare_ext3_testing.sh
+++ tests/acceptance/prepare_ext3_testing.sh
@@ -6,13 +6,17 @@ DST_PATH=${2:-`pwd`"/core-image-base-beaglebone-modified-testing.ext3"}
 cp ${SRC_PATH} ${DST_PATH}
 
 
-if [ ! `ls files/keys/id_rsa* | wc -l` -eq 2 ];
+# These files are found inside the tests/acceptance/files/keys/*.gpg archive.
+# However, since it is needed for all workspaces, and we don't want to extract
+# them for each and every one, they should be extracted in $HOME/files/keys,
+# where they will be picked up.
+if [ ! `ls $HOME/files/keys/id_rsa* | wc -l` -eq 2 ];
   then
     echo "ssh keys not found" 1>&2
     exit 1
 fi
 
-e2cp -P 400 files/keys/{id_rsa,id_rsa.pub} ${DST_PATH}:/home/root/.ssh/
+e2cp -P 400 $HOME/files/keys/{id_rsa,id_rsa.pub} ${DST_PATH}:/home/root/.ssh/
 e2cp files/rssh.service ${DST_PATH}:/etc/systemd/system/multi-user.target.wants/
 echo "linking image.dat to ${DST_PATH}"
 ln -s -f ${DST_PATH} `dirname ${DST_PATH}`/image.dat
