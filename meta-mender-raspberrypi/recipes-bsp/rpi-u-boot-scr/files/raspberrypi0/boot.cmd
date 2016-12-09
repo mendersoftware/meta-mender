5edd32db1595a02c590331c0bf82115374fad747
Mirza Krak <mirza.krak@gmail.com>
2016-12-09 19:16:50 +0100

    raspberrypi: fix console port on Linux
    The default clock rate has now been updated upstream so we can drop our
    workaround.
    
    It also now requires to be explicit about bauderate in console kernel
    argument.
    
    Signed-off-by: Mirza Krak <mirza.krak@gmail.com>

--- meta-mender-raspberrypi/recipes-bsp/rpi-u-boot-scr/files/raspberrypi0/boot.cmd
+++ meta-mender-raspberrypi/recipes-bsp/rpi-u-boot-scr/files/raspberrypi0/boot.cmd
@@ -1,6 +1,6 @@
 run mender_setup
 setenv fdtfile bcm2708-rpi-b.dtb
-setenv bootargs earlyprintk console=tty0 console=ttyAMA0 root=${mender_kernel_root} rootfstype=ext4 rootwait noinitrd
+setenv bootargs earlyprintk console=tty0 console=ttyAMA0,115200 root=${mender_kernel_root} rootfstype=ext4 rootwait noinitrd
 mmc dev ${mender_uboot_dev}
 load ${mender_uboot_root} ${kernel_addr_r} /boot/uImage
 load ${mender_uboot_root} ${fdt_addr_r} /boot/${fdtfile}
