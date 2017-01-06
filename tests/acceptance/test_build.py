e3012a79401d47d02fe6a2245fe0f65c1e3fce06
Kristian Amlie <kristian.amlie@mender.io>
2017-01-06 12:56:28 +0100

    Add test to make sure our server certificate check stays up to date.
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>

---
+++ tests/acceptance/test_build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# Copyright 2016 Mender Software AS
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import pytest
+import subprocess
+
+# Make sure common is imported after fabric, because we override some functions.
+from common import *
+
+class TestBuild:
+    def test_default_server_certificate(self):
+        """Test that the md5sum we have on record matches the server certificate.
+        This makes sure the warning about this certificate is correct."""
+
+        output = subprocess.check_output(["md5sum", "../../meta-mender-core/recipes-mender/mender/files/server.crt"])
+
+        # Crude check, just make sure it occurs in the build file.
+        subprocess.check_call("fgrep %s ../../meta-mender-core/recipes-mender/mender/mender_*.bb >/dev/null 2>&1"
+                              % output.split()[0], shell=True)
