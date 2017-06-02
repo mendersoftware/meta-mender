bdbec253b94267e3e81aeeb9d042367c150245fd
Kristian Amlie <kristian.amlie@mender.io>
2017-06-02 09:02:27 +0200

    QEMU: Add ability to configure certain client values before launching
    * SERVER_URL and TENANT_TOKEN environment variable can be passed in
      while launching
    
    * server.crt can be passed in by mounting a volume containing it on
      /mnt/config.
    
    More info in the Dockerfile for QEMU.
    
    MEN-1207
    
    Changelog: Commit
    
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>

---
+++ meta-mender-qemu/scripts/docker/setup-mender-configuration.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import subprocess
+
+def get(remote_path, local_path, rootfs):
+    subprocess.check_call(["debugfs", "-R", "dump -p %s %s" % (remote_path, local_path), rootfs],
+                          stderr=subprocess.STDOUT)
+
+def put(local_path, remote_path, rootfs):
+    proc = subprocess.Popen(["debugfs", "-w", rootfs], stdin=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    proc.stdin.write(("cd %s\n" % os.path.dirname(remote_path)).encode())
+    proc.stdin.write(("rm %s\n" % os.path.basename(remote_path)).encode())
+    proc.stdin.write(("write %s %s\n" % (local_path, os.path.basename(remote_path))).encode())
+    proc.stdin.close()
+    ret = proc.wait()
+    assert ret == 0
+
+def extract_ext4(sdimg, rootfs):
+    return manipulate_ext4(sdimg=sdimg, rootfs=rootfs, write=False)
+
+def insert_ext4(sdimg, rootfs):
+    return manipulate_ext4(sdimg=sdimg, rootfs=rootfs, write=True)
+
+def manipulate_ext4(sdimg, rootfs, write):
+    output = subprocess.check_output(["fdisk", "-ul", sdimg])
+    for line in output.decode().split('\n'):
+        columns = line.split()
+        if len(columns) < 3:
+            continue
+        # This blindly assumes that rootfs is on partition 2.
+        if columns[0] == ("%s2" % sdimg):
+            if write:
+                subprocess.check_call(["dd", "if=%s" % rootfs, "of=%s" % sdimg,
+                                       "seek=%s" % columns[1],
+                                       "count=%d" % (int(columns[2]) - int(columns[1]) + 1),
+                                       "conv=notrunc"],
+                                      stderr=subprocess.STDOUT)
+            else:
+                subprocess.check_call(["dd", "if=%s" % sdimg, "of=%s" % rootfs,
+                                       "skip=%s" % columns[1],
+                                       "count=%d" % (int(columns[2]) - int(columns[1]) + 1)],
+                                      stderr=subprocess.STDOUT)
+            break
+    else:
+        raise Exception("%s not found in fdisk output: %s" % (sdimg, output))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sdimg", help="Sdimg to modify")
+    parser.add_argument("--tenant-token", help="tenant token to use by client")
+    parser.add_argument("--server-crt", help="server.crt file to put in image")
+    parser.add_argument("--server-url", help="Server address to put in configuration")
+    args = parser.parse_args()
+
+    if not (args.tenant_token or args.server_crt or args.server_url):
+        return
+
+    # Extract ext4 image from sdimg.
+    assert args.sdimg, "--sdimg needs to be supplied"
+    rootfs = "%s.ext4" % args.sdimg
+    extract_ext4(sdimg=args.sdimg, rootfs=rootfs)
+
+    if args.tenant_token:
+        with open("tenant.conf", "w") as fd:
+            fd.write("%s\n" % args.tenant_token)
+        put(local_path="tenant.conf",
+            remote_path="/etc/mender/tenant.conf",
+            rootfs=rootfs)
+        os.unlink("tenant.conf")
+
+    if args.server_crt:
+        put(local_path=args.server_crt,
+            remote_path="/etc/mender/server.crt",
+            rootfs=rootfs)
+
+    if args.server_url:
+        get(local_path="mender.conf",
+            remote_path="/etc/mender/mender.conf",
+            rootfs=rootfs)
+        with open("mender.conf") as fd:
+            conf = json.load(fd)
+        conf['ServerURL'] = args.server_url
+        with open("mender.conf", "w") as fd:
+            json.dump(conf, fd, indent=4, sort_keys=True)
+        put(local_path="mender.conf",
+            remote_path="/etc/mender/mender.conf",
+            rootfs=rootfs)
+        os.unlink("mender.conf")
+
+    # Put back ext4 image into sdimg.
+    insert_ext4(sdimg=args.sdimg, rootfs=rootfs)
+    os.unlink(rootfs)
+
+main()
