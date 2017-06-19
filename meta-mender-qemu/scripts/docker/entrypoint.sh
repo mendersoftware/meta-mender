4f2810f52404cd57491388db447d069d2cedd8a8
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-06-19 12:47:59 +0200

    meta-mender-qemu/docker: support vexpress-nor images, use VEXPRESS_IMG
    Update Dockerfile and entrypoint to support vexpress-qemu-flash machine with
    vexpress-nor images. Also properly set VEXPRESS_IMG (previously VEXPRESS_SDIMG)
    to pass the location of image file.
    
    Changelog: title
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>

--- meta-mender-qemu/scripts/docker/entrypoint.sh
+++ meta-mender-qemu/scripts/docker/entrypoint.sh
@@ -2,11 +2,31 @@
 
 set -x -e
 
-if [ -e /mnt/build/tmp/deploy/images/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg ]; then
-    cp /mnt/build/tmp/deploy/images/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg .
-fi
-if [ -e /mnt/build/tmp/deploy/images/vexpress-qemu/u-boot.elf ]; then
-    cp /mnt/build/tmp/deploy/images/vexpress-qemu/u-boot.elf .
+MACHINE=${MACHINE:-"vexpress-qemu"}
+IMAGE=${IMAGE:-"core-image-full-cmdline"}
+
+case "$MACHINE" in
+    vexpress-qemu)
+        if [ -e /mnt/build/tmp/deploy/images/${MACHINE}/${IMAGE}-${MACHINE}.sdimg ]; then
+            cp /mnt/build/tmp/deploy/images/${MACHINE}/${IMAGE}-${MACHINE}.sdimg .
+        fi
+        VEXPRESS_IMG="${IMAGE}-${MACHINE}.sdimg"
+        ;;
+    vexpress-qemu-flash)
+        if [ -e /mnt/build/tmp/deploy/images/${MACHINE}/${IMAGE}-${MACHINE}.vexpress-nor ]; then
+            cp /mnt/build/tmp/deploy/images/${MACHINE}/${IMAGE}-${MACHINE}.vexpress-nor .
+        fi
+        VEXPRESS_IMG="${IMAGE}-${MACHINE}.vexpress-nor"
+        ;;
+    *)
+        echo "unsupported machine $MACHINE"
+        exit 1
+        ;;
+esac
+
+# NOTE: vexpress-qemu-flash has a differently configured u-boot
+if [ -e /mnt/build/tmp/deploy/images/${MACHINE}/u-boot.elf ]; then
+    cp /mnt/build/tmp/deploy/images/${MACHINE}/u-boot.elf .
 fi
 
 CONFIG_ARGS=
@@ -19,6 +39,15 @@ if [ -f /mnt/config/artifact-verify-key.pem ]; then
     CONFIG_ARGS="$CONFIG_ARGS --verify-key=/mnt/config/artifact-verify-key.pem"
 fi
 
-./setup-mender-configuration.py --sdimg=core-image-full-cmdline-vexpress-qemu.sdimg --server-url=$SERVER_URL --tenant-token=$TENANT_TOKEN $CONFIG_ARGS
+if [ "$MACHINE" = "vexpress-qemu "]; then
+    ./setup-mender-configuration.py --sdimg=core-image-full-cmdline-vexpress-qemu.sdimg \
+                                    --server-url=$SERVER_URL \
+                                    --tenant-token=$TENANT_TOKEN $CONFIG_ARGS
+else
+    if [ -n "$TENANT_TOKEN" -o -n "$CONFIG_ARGS" -o -n "$SERVER_URL" ]; then
+       echo "reconfiguration of flash images is not supported"
+       exit 1
+    fi
+fi
 
-QEMU_SYSTEM_ARM="qemu-system-arm" VEXPRESS_SDIMG="core-image-full-cmdline-vexpress-qemu.sdimg" UBOOT_ELF="u-boot.elf" ./mender-qemu $*
+QEMU_SYSTEM_ARM="qemu-system-arm" VEXPRESS_IMG="${VEXPRESS_IMG}" UBOOT_ELF="u-boot.elf" ./mender-qemu $*
