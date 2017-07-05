b4d81f15d0fe534d559f440f2f124771ee77c7f1
Mirza Krak <mirza.krak@endian.se>
2017-07-05 14:22:52 +0200

    raspberrypi: use bootargs passed on by boot firmware
    Currently we completly ignore what boot firmware feels that we
    should use as bootargs and we define our own. By doing so we
    lose some functionality. This patch attempts to improve this.
    
    The bootargs looks like this now:
    
    8250.nr_uarts=0 bcm2708_fb.fbwidth=1184 bcm2708_fb.fbheight=624 bcm2708_fb.fbswap=1 vc_mem.mem_base=0x3dc00000 vc_mem.mem_size=0x3f000000 dwc_otg.lpm_enable=0 console=ttyS0,115200 rootfstype=ext4
    rootwait root=/dev/mmcblk0p3
    
    Changelog: Title
    
    Signed-off-by: Mirza Krak <mirza.krak@endian.se>
    (cherry picked from commit fa65709b66557afaf3f0e3e0f34aba9dbbe4f677)

--- meta-mender-raspberrypi/recipes-bsp/rpi-u-boot-scr/files/raspberrypi3/boot.cmd
+++ meta-mender-raspberrypi/recipes-bsp/rpi-u-boot-scr/files/raspberrypi3/boot.cmd
@@ -1,8 +1,8 @@
-run mender_setup
 setenv fdtfile bcm2710-rpi-3-b.dtb
-setenv bootargs earlyprintk console=tty0 console=ttyAMA0,115200 root=${mender_kernel_root} rootfstype=ext4 rootwait noinitrd
+fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
+run mender_setup
 mmc dev ${mender_uboot_dev}
 load ${mender_uboot_root} ${kernel_addr_r} /boot/uImage
-load ${mender_uboot_root} ${fdt_addr_r} /boot/${fdtfile}
-bootm ${kernel_addr_r} - ${fdt_addr_r}
+load ${mender_uboot_root} ${fdt_addr} /boot/${fdtfile}
+bootm ${kernel_addr_r} - ${fdt_addr}
 run mender_try_to_recover
