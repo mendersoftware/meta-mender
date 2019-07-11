a0896ec2c79beda2c352b5bbc548a78a4d806c15
Kristian Amlie <kristian.amlie@northern.tech>
2019-07-11 10:43:29 +0200

    Squashed 'tests/acceptance/3rdparty/mendertesting/' changes from 731be57..fc34c8e
    fc34c8e Merge pull request #29 from mendersoftware/gitlab
    9068ca0 Fix commit range detection for Gitlab builds yet again.
    c082253 Merge pull request #28 from kacf/gitlab
    19e7aa9 Make sure correct revisions are checked in Gitlab builds.
    6b02f96 Merge pull request #27 from kacf/master
    dedd2c0 Use --follow when checking last modified year of file.
    3bac1d2 Merge pull request #26 from lluiscampos/fix-commit-range-branch-build
    e9e6d9b Update license year in test (and test file)
    9fdd269 Update license year in top-level LICENSE file
    8005c3c Modify go vet call, use the same args as in the other repos
    f89075a Remove testing go version latest
    9e728b5 Merge pull request #24 from kacf/master
    59c64e8 Add checking of copyright year in main LICENSE file.
    90e4cd4 Merge pull request #23 from kacf/master
    1c608df Fix missing error return if failing license check.
    7b213eb Fix typo.
    e9053da Merge pull request #22 from pasinskim/master
    e5402ae Update license check to check for specific copyright year.
    
    git-subtree-dir: tests/acceptance/3rdparty/mendertesting
    git-subtree-split: fc34c8e8a47a90de86a6cfa62ad0fb607c9d3078

--- check_commits.sh
+++ check_commits.sh
@@ -9,10 +9,32 @@ case "$1" in
         ;;
 esac
 
-if [ -n "$TRAVIS_BRANCH" ]
+if [ -z "$COMMIT_RANGE" ] && [ -n "$CI_COMMIT_REF_NAME" ]
+then
+    # Gitlab unfortunately doesn't record base branches of commits when the PR
+    # comes from Github, so we need to detect branch names of PRs manually, and
+    # then reconstruct the correct range from that, by excluding all other
+    # branches.
+    case "$CI_COMMIT_REF_NAME" in
+        pr_[0-9]*)
+            EXCLUDE_LIST=$(mktemp)
+            EXCLUDE_LIST_REMOVE=$(mktemp)
+            git for-each-ref --format='%(refname)' | sort > $EXCLUDE_LIST
+            git for-each-ref --format='%(refname)' --points-at $CI_COMMIT_REF_NAME | sort > $EXCLUDE_LIST_REMOVE
+            TO_EXCLUDE="$(comm -23 $EXCLUDE_LIST $EXCLUDE_LIST_REMOVE | tr '\n' ' ')"
+            COMMIT_RANGE="$CI_COMMIT_REF_NAME --not $TO_EXCLUDE"
+            rm -f $EXCLUDE_LIST $EXCLUDE_LIST_REMOVE
+            ;;
+    esac
+fi
+
+if [ -z "$COMMIT_RANGE" ] && [ -n "$TRAVIS_BRANCH" ]
 then
     COMMIT_RANGE="$TRAVIS_BRANCH..HEAD"
-else
+fi
+
+if [ -z "$COMMIT_RANGE" ]
+then
     # Just check previous commit if nothing else is specified.
     COMMIT_RANGE=HEAD~1..HEAD
 fi
@@ -24,8 +46,8 @@ then
     commits="$(git rev-list --no-merges "$@")"
 else
     echo "Checking range: ${COMMIT_RANGE}:"
-    git --no-pager log "$COMMIT_RANGE"
-    commits="$(git rev-list --no-merges "$COMMIT_RANGE")"
+    git --no-pager log $COMMIT_RANGE
+    commits="$(git rev-list --no-merges $COMMIT_RANGE)"
 fi
 notvalid=
 for i in $commits
