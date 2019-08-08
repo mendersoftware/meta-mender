97eccba8d9e0b54bc0bdf24401183840e2e08029
Drew Moseley <drew.moseley@northern.tech>
2019-08-08 01:11:03 +0000

    mender: Scan for devices with the live installer
    In the case of using mender-partuuid, MENDER_STORAGE_DEVICE
    is unused. We need to scan for available block devices
    in that case.
    
    Changelog: Title
    
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>
    (cherry picked from commit 6708b38dd1650441a747a7a3e451aef6fae15f5c)

--- meta-mender-core/recipes-core/initrdscripts/files/init-install-efi-mender.sh
+++ meta-mender-core/recipes-core/initrdscripts/files/init-install-efi-mender.sh
@@ -4,33 +4,67 @@ set -ue
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 devnode=@MENDER_STORAGE_DEVICE@
+bootdev="$(echo /dev/$1 | sed 's@/$@@')"
 
-if [ -b ${devnode} ]; then
+if [ -z ${devnode} ]; then
+    echo "Searching for hard drives ..."
+    devlist=$(fdisk -l 2>&1 | \
+                  grep ^Disk\ /dev | \
+                  grep -v loop | \
+                  grep -v sr0 | \
+                  grep -v mapper | \
+                  grep -v ram | \
+                  grep -v 'mmcblk.*boot' | \
+                  grep -v ${bootdev} | \
+                  cut -d: -f 1 | cut -d\  -f 2)
+    for dev in ${devlist}; do
+        devbase=$(echo $dev | sed 's@/dev/@@')
+        echo "Detected block device " ${dev}
+        [ -r /sys/block/$devbase/device/vendor ] && echo "VENDOR=$(cat /sys/block/$devbase/device/vendor)"
+        [ -r /sys/block/$devbase/device/model  ] && echo "MODEL=$(cat /sys/block/$devbase/device/model)"
+        [ -r /sys/block/$devbase/device/uevent ] && echo "UEVENT=$(cat /sys/block/$devbase/device/uevent)"
+        echo -n "Install to this device? <y/n> "
+        read yn
+        if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
+            devnode=$dev
+            break
+        fi
+    done
+fi
+
+if [ -n "${devnode}" -a -b ${devnode} ]; then
     echo "Installing image on ${devnode} ..."
 else
     echo "No device available matching ${devnode}. Installation aborted."
+    /bin/sh
     exit 1
 fi
 
-if [ -e /run/media/$1/$2 ]; then
-    echo "Writing Mender-enabled Yocto image /run/media/$1/$2 to ${devnode}"
-    echo "bzcat /run/media/$1/$2 | /bin/dd of=$devnode bs=8M"
+if [ -e /run/media/$1$2 ]; then
+    echo "Writing Mender-enabled Yocto image /run/media/$1$2 to ${devnode}"
+    echo "bzcat /run/media/$1$2 | /bin/dd of=$devnode bs=8M"
     echo -n "OK to proceed? <y/n> "
     read yn
     if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
         rc=0
-        bzcat /run/media/$1/$2 | /bin/dd of=$devnode bs=8M || rc=$?
+        bzcat /run/media/$1$2 | /bin/dd of=$devnode bs=8M || rc=$?
         sync
         if [ $rc = 0 ]; then
             echo "Installation successful."
         else
             echo "/bin/dd returned error $rc. Review above log. Installation aborted"
+            /bin/sh
+            exit 1
         fi
     else
         echo "Installation aborted"
+        /bin/sh
+        exit 1
     fi
 else
-    echo "Unable to locate $2. Installation aborted"
+    echo "Unable to locate image $2, on bootdev ${bootdev}. Installation aborted"
+    /bin/sh
+    exit 1
 fi
 
 echo "Remove your installation media and press ENTER to reboot."
