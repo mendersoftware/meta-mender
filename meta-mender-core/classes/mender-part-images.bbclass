mendersoftware/meta-mender

commit 0b737428d0570de16935e0758db7752de4134d2a

Allow boot partition to be populated with normal package recipes.
This was implemented some time ago for the data partition, but the
boot partition could still only be populated with IMAGE_BOOT_FILES.
IMAGE_BOOT_FILES is still widely used and a standard Yocto variable,
so we keep support for that as well.

Changelog: Title

Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

meta-mender-core/classes/mender-part-images.bbclass
@@ -41,6 +41,47 @@ IMAGE_NAME_SUFFIX = ""
 # Block storage
 ################################################################################
 
+# Take the content from the rootfs that is going into the boot partition, coming
+# from MENDER_BOOT_PART_MOUNT_LOCATION, and merge with the files from
+# IMAGE_BOOT_FILES, following the format from the official Yocto documentation.
+mender_merge_bootfs_and_image_boot_files() {
+    W="${WORKDIR}/bootfs.${BB_CURRENTTASK}"
+    rm -rf "$W"
+
+    cp -al "${IMAGE_ROOTFS}/${MENDER_BOOT_PART_MOUNT_LOCATION}" "$W"
+
+    # Put in variable to avoid expansion and ';' being parsed by shell.
+    image_boot_files="${IMAGE_BOOT_FILES}"
+    for entry in $image_boot_files; do
+        if echo "$entry" | grep -q ";"; then
+            dest="$(echo "$entry" | sed -e 's/[^;]*;//')"
+            entry="$(echo "$entry" | sed -e 's/;.*//')"
+        else
+            dest="./"
+        fi
+        if echo "$dest" | grep -q '/$'; then
+            dest_is_dir=1
+            mkdir -p "$W/$dest"
+        else
+            dest_is_dir=0
+            mkdir -p "$(dirname "$W/$dest")"
+        fi
+
+        # Use extra for loop so we can check conflict for each file.
+        for file in ${DEPLOY_DIR_IMAGE}/$entry; do
+            if [ $dest_is_dir -eq 1 ]; then
+                destfile="$W/$dest$(basename $file)"
+            else
+                destfile="$W/$dest"
+            fi
+            if [ -e "$destfile" ]; then
+                bbfatal "$destfile already exists in boot partition. Please verify that packages do not put files in the boot partition that conflict with IMAGE_BOOT_FILES."
+            fi
+            cp -l "$file" "$destfile"
+        done
+    done
+}
+
 mender_part_image() {
     suffix="$1"
     part_type="$2"
@@ -105,9 +146,12 @@ EOF
     alignment_kb=$(expr ${MENDER_PARTITION_ALIGNMENT} / 1024)
 
     if [ "${MENDER_BOOT_PART_SIZE_MB}" -ne "0" ]; then
+        mender_merge_bootfs_and_image_boot_files
         cat >> "$wks" <<EOF
-part $boot_part_params --ondisk "$ondisk_dev" --fstype=vfat --label boot --align $alignment_kb --active --fixed-size ${MENDER_BOOT_PART_SIZE_MB}
+part --source rootfs --rootfs-dir ${WORKDIR}/bootfs.${BB_CURRENTTASK} --ondisk "$ondisk_dev" --fstype=vfat --label boot --align $alignment_kb --fixed-size ${MENDER_BOOT_PART_SIZE_MB}
 EOF
+    elif [ -n "${IMAGE_BOOT_FILES}" ]; then
+        bbwarn "MENDER_BOOT_PART_SIZE_MB is set to zero, but IMAGE_BOOT_FILES is not empty. The files are being omitted from the image."
     fi
 
     exclude_dirs_options="${@" ".join(["--exclude-path %s" % dir for dir in (d.getVar("IMAGE_ROOTFS_EXCLUDE_PATH") or "").split()])}"

meta-mender-core/classes/mender-setup-image.inc
@@ -45,7 +45,7 @@ mender_create_scripts_version_file() {
     echo -n "${MENDER_STATE_SCRIPTS_VERSION}" > ${IMAGE_ROOTFS}${sysconfdir}/mender/scripts/version
 }
 
-IMAGE_ROOTFS_EXCLUDE_PATH_append_mender-image = " data/"
+IMAGE_ROOTFS_EXCLUDE_PATH_append_mender-image = " data/ ${@d.getVar('MENDER_BOOT_PART_MOUNT_LOCATION')[1:]}"
 
 
 ################################################################################

tests/acceptance/test_part_image.py
@@ -237,3 +237,48 @@ class TestPartitionImage:
             finally:
                 os.fchdir(old_cwd_fd)
                 os.close(old_cwd_fd)
+
+
+    @pytest.mark.min_mender_version('1.0.0')
+    def test_boot_partition_population(self, prepared_test_build, bitbake_path):
+        # Notice in particular a mix of tabs, newlines and spaces. All there to
+        # check that whitespace it treated correctly.
+        add_to_local_conf(prepared_test_build, """
+IMAGE_INSTALL_append = " test-boot-files"
+
+IMAGE_BOOT_FILES_append = " deployed-test1 deployed-test-dir2/deployed-test2 \
+	deployed-test3;renamed-deployed-test3 \
+ deployed-test-dir4/deployed-test4;renamed-deployed-test4	deployed-test5;renamed-deployed-test-dir5/renamed-deployed-test5 \
+deployed-test-dir6/deployed-test6;renamed-deployed-test-dir6/renamed-deployed-test6 \
+deployed-test-dir7/* \
+deployed-test-dir8/*;./ \
+deployed-test-dir9/*;renamed-deployed-test-dir9/ \
+"
+""")
+        run_bitbake(prepared_test_build)
+
+        image = latest_build_artifact(prepared_test_build['build_dir'], "core-image*.*img")
+        extract_partition(image, 1)
+        try:
+            listing = run_verbose("mdir -i img1.fs -b -/", capture=True).split()
+            expected = [
+                "::/deployed-test1",
+                "::/deployed-test2",
+                "::/renamed-deployed-test3",
+                "::/renamed-deployed-test4",
+                "::/renamed-deployed-test-dir5/renamed-deployed-test5",
+                "::/renamed-deployed-test-dir6/renamed-deployed-test6",
+                "::/deployed-test7",
+                "::/deployed-test8",
+                "::/renamed-deployed-test-dir9/deployed-test9",
+            ]
+            assert(all([item in listing for item in expected]))
+
+            add_to_local_conf(prepared_test_build, 'IMAGE_BOOT_FILES_append = " conflict-test1"')
+            try:
+                run_bitbake(prepared_test_build)
+                pytest.fail("Bitbake succeeded, but should have failed with a file conflict")
+            except subprocess.CalledProcessError:
+                pass
+        finally:
+            os.remove("img1.fs")

tests/meta-mender-ci/recipes-testing/test-boot-files/files/dummy
@@ -1,0 +1,1 @@
+test file

tests/meta-mender-ci/recipes-testing/test-boot-files/test-boot-files_1.0.bb
@@ -1,0 +1,39 @@
+SRC_URI = "file://dummy"
+LICENSE = "PD"
+LIC_FILES_CHKSUM = "file://dummy;md5=b05403212c66bdc8ccc597fedf6cd5fe"
+S = "${WORKDIR}"
+
+FILES_${PN} = "${MENDER_BOOT_PART_MOUNT_LOCATION}"
+
+inherit deploy
+
+do_install() {
+    install -d -m 755 ${D}${MENDER_BOOT_PART_MOUNT_LOCATION}
+    install -m 644 ${WORKDIR}/dummy ${D}${MENDER_BOOT_PART_MOUNT_LOCATION}/installed-test1
+    install -d -m 755 ${D}${MENDER_BOOT_PART_MOUNT_LOCATION}/installed-test-dir2
+    install -m 644 ${WORKDIR}/dummy ${D}${MENDER_BOOT_PART_MOUNT_LOCATION}/installed-test-dir2/installed-test2
+
+    install -m 644 ${WORKDIR}/dummy ${D}${MENDER_BOOT_PART_MOUNT_LOCATION}/conflict-test1
+}
+
+do_deploy() {
+    install -d -m 755 ${DEPLOYDIR}
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test1
+    install -d -m 755 ${DEPLOYDIR}/deployed-test-dir2
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test-dir2/deployed-test2
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test3
+    install -d -m 755 ${DEPLOYDIR}/deployed-test-dir4
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test-dir4/deployed-test4
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test5
+    install -d -m 755 ${DEPLOYDIR}/deployed-test-dir6
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test-dir6/deployed-test6
+    install -d -m 755 ${DEPLOYDIR}/deployed-test-dir7
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test-dir7/deployed-test7
+    install -d -m 755 ${DEPLOYDIR}/deployed-test-dir8
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test-dir8/deployed-test8
+    install -d -m 755 ${DEPLOYDIR}/deployed-test-dir9
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/deployed-test-dir9/deployed-test9
+
+    install -m 644 ${WORKDIR}/dummy ${DEPLOYDIR}/conflict-test1
+}
+addtask do_deploy

commit 4eb39076050b08c3a9ce5a335914fb1bd1f45459

Fix incorrect boot partition type for EFI boot partitions.
This was broken in 0b737428d0570de16935e0758db7752de4134d2a, where we
lost the `boot_part_params` along the way.

Changelog: Title

Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

meta-mender-core/classes/mender-part-images.bbclass
@@ -111,7 +111,7 @@ EOF
     if [ "${MENDER_BOOT_PART_SIZE_MB}" -ne "0" ]; then
         mender_merge_bootfs_and_image_boot_files
         cat >> "$wks" <<EOF
-part --source rootfs --rootfs-dir ${WORKDIR}/bootfs.${BB_CURRENTTASK} --ondisk "$ondisk_dev" --fstype=vfat --label boot --align $alignment_kb --fixed-size ${MENDER_BOOT_PART_SIZE_MB} --active
+part --source rootfs --rootfs-dir ${WORKDIR}/bootfs.${BB_CURRENTTASK} --ondisk "$ondisk_dev" --fstype=vfat --label boot --align $alignment_kb --fixed-size ${MENDER_BOOT_PART_SIZE_MB} --active $boot_part_params
 EOF
     elif [ -n "$IMAGE_BOOT_FILES_STRIPPED" ]; then
         bbwarn "MENDER_BOOT_PART_SIZE_MB is set to zero, but IMAGE_BOOT_FILES is not empty. The files are being omitted from the image."
@@ -194,16 +194,16 @@ EOF
 }
 
 IMAGE_CMD_sdimg() {
-    mender_part_image sdimg msdos "--source bootimg-partition"
+    mender_part_image sdimg msdos
 }
 IMAGE_CMD_uefiimg() {
-    mender_part_image uefiimg gpt "--source bootimg-partition --part-type EF00"
+    mender_part_image uefiimg gpt "--part-type EF00"
 }
 IMAGE_CMD_biosimg() {
-    mender_part_image biosimg msdos "--source bootimg-partition"
+    mender_part_image biosimg msdos
 }
 IMAGE_CMD_gptimg() {
-    mender_part_image gptimg gpt "--source bootimg-partition"
+    mender_part_image gptimg gpt
 }
 
 