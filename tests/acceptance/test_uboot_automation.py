fcb0da894f566f75a26109bec24e44b867efa890
Kristian Amlie <kristian.amlie@northern.tech>
2019-10-14 14:01:34 +0200

    test_uboot_automation: Update failed board count for Flash tests.
    The whole thing is an inexact science, but it looks like the majority
    of failing boards are because a few select boards have been added with
    different configurations, so they keep failing over and over. I could
    not spot any serious regressions, so updating the test number instead.
    
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- tests/acceptance/test_uboot_automation.py
+++ tests/acceptance/test_uboot_automation.py
@@ -279,8 +279,8 @@ class TestUbootAutomation:
                 measured_failed_ratio = 198.0 / 664.0
             elif machine == "vexpress-qemu-flash":
                 # PLEASE UPDATE the version you used to find this number if you update it.
-                # From version: v2018.05
-                measured_failed_ratio = 36.0 / 159.0
+                # From version: v2019.01
+                measured_failed_ratio = 53.0 / 185.0
 
             # We tolerate a certain percentage discrepancy in either direction.
             tolerated_discrepancy = 0.1
