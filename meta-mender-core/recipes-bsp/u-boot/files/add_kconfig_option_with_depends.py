b070331495e9659a0b47f091bcbead37c4e2fc64
Joerg Hofrichter <joerg.hofrichter@ni.com>
2019-11-21 13:39:52 +0100

    uboot_auto_patch: handle more complex dependency strings in add_kconfig_option_with_depends script
    The change makes the script capable of handling more complex depends-strings which
    contain or-operators ("||") where the first alternative is used, parentheses, and
    and-operators ("&&").
    
    The "boolean" library from Sebastian Kraemer is used for evaluating the logical expressions.
    It was copied from https://github.com/bastikr/boolean.py/blob/master/boolean/boolean.py
    (dd4bc01) as there is no python3-boolean recipe available for the current openembedded releases.
    
    This makes the script compatible with U-Boot 2019.10.
    
    U-Boot 2019.10 has the following ENV_OFFSET dependencies:
        depends on (!ENV_IS_IN_UBI && !ENV_IS_NOWHERE) || ARCH_STM32MP
    (see https://gitlab.denx.de/u-boot/u-boot/blob/v2019.10/env/Kconfig#L469)
    
    In comparison, U-Boot 2019.01 has the following ENV_OFFSET dependencies:
        depends on !ENV_IS_IN_UBI
        depends on !ENV_IS_NOWHERE
    (see https://gitlab.denx.de/u-boot/u-boot/blob/v2019.01/env/Kconfig#L436)
    
    Changelog: Update uboot_auto_patch script to be compatible with U-Boot 2019.10
    
    Signed-off-by: Joerg Hofrichter <joerg.hofrichter@ni.com>

--- meta-mender-core/recipes-bsp/u-boot/files/add_kconfig_option_with_depends.py
+++ meta-mender-core/recipes-bsp/u-boot/files/add_kconfig_option_with_depends.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import re
+import boolean
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--src-dir", required=True,
@@ -15,6 +16,51 @@ parser.add_argument("--defconfig-file", required=True,
 parser.add_argument("option", metavar="OPTION", nargs=1,
                     help="Option to add to Kconfig, expressed as KEY=VALUE.")
 args = parser.parse_args()
+
+def term_parse(term_string):
+    term_string = term_string.replace("||","OR")
+    term_string = term_string.replace("&&","AND")
+    algebra = boolean.BooleanAlgebra()
+    return algebra.parse(term_string)
+
+def term_reduce_to_first_alternative(term):
+    if type(term) == boolean.Symbol:
+        # return symbol as is
+        return term
+    elif type(term) == boolean.NOT:
+        # return Nonetype (do not handle negated terms)
+        return None
+    elif type(term) == boolean.OR:
+        # return first alternative
+        return term_reduce_to_first_alternative(term.args[0])
+    elif type(term) == boolean.AND:
+        newterm = ()
+        for subterm in term.args:
+            newsubterm = term_reduce_to_first_alternative(subterm)
+            if newsubterm != None:
+                newterm += (newsubterm, )
+        if len(newterm) == 0:
+            return None
+        elif len(newterm) == 1:
+            return newterm[0]
+        else:
+            return boolean.AND(*newterm)
+    else:
+        raise ValueError
+
+def term_to_string_list(term):
+    items = []
+    if type(term) == boolean.Symbol:
+        items.append(str(term))
+    elif type(term) == boolean.AND:
+        for arg in term.args:
+            items.append(str(arg))
+    return items
+
+def parse_dependencies(depends_string):
+    depends_orig = term_parse(depends_string)
+    depends_reduced = term_reduce_to_first_alternative(depends_orig)
+    return term_to_string_list(depends_reduced)
 
 def add_kconfig_option(option):
     key, value = option.split("=", 1)
@@ -44,14 +90,11 @@ def add_kconfig_option(option):
                     if not inside_option:
                         continue
 
-                    match = re.match("^\s*depends *on *(\S+)", line)
+                    match = re.match("^\s*depends\s*on\s*(.+)", line)
                     if not match:
                         continue
-                    dependee = match.group(1)
-                    if dependee.startswith("!"):
-                        # We're not handling negative dependencies right now.
-                        continue
-                    add_kconfig_option("CONFIG_%s=y" % dependee)
+                    for dependee in parse_dependencies(match.group(1)):
+                        add_kconfig_option("CONFIG_%s=y" % dependee)
 
     with open(args.defconfig_file, "a") as fd:
         fd.write("%s\n" % option)
