6b03a1aff7d627858d05a5d3cea013412f73ccb5
Kristian Amlie <kristian.amlie@northern.tech>
2020-07-01 10:04:23 +0200

    test_uboot_compile: Update test conditions after fixes in this area.
    One large source of improved ratio is that we now exclude ARM64 boards
    properly, which eliminates a lot of false negatives.
    
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>
    (cherry picked from commit a5d615791b43bd7f2647d3dabe88be0601bdfb76)

--- tests/acceptance/test_uboot_automation.py
+++ tests/acceptance/test_uboot_automation.py
@@ -337,12 +337,12 @@ class TestUbootAutomation:
 
             if machine == "vexpress-qemu":
                 # PLEASE UPDATE the version you used to find this number if you update it.
-                # From version: v2018.05
-                measured_failed_ratio = 198.0 / 664.0
+                # From version: v2020.01
+                measured_failed_ratio = 57.0 / 561.0
             elif machine == "vexpress-qemu-flash":
                 # PLEASE UPDATE the version you used to find this number if you update it.
-                # From version: v2019.01
-                measured_failed_ratio = 53.0 / 185.0
+                # From version: v2020.01
+                measured_failed_ratio = 13.0 / 143.0
 
             # We tolerate a certain percentage discrepancy in either direction.
             tolerated_discrepancy = 0.1
