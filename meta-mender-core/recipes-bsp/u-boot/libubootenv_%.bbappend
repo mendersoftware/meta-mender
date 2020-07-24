mendersoftware/meta-mender

commit f61fa534d43dcd4edb485879c96d9a425b4d05cc

Start using libubootenv for U-Boot environment manipulation.
This deprecates the u-boot-fw-utils tools, which have been removed
from upstream. The functionality and command line API is the same.

Changelog: Commit

Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

meta-mender-core/recipes-bsp/u-boot/libubootenv_%.bbappend
@@ -1,0 +1,56 @@
+include u-boot-mender-helpers.inc
+
+FILES_${PN}_append_mender-uboot = " /data/u-boot/fw_env.config"
+
+mender_create_fw_env_config_file() {
+    # Takes one argument, which is the file to put it in.
+
+    set -x
+
+    # fw-utils seem to only be able to handle hex values.
+    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
+
+    # create fw_env.config file
+    cat > $1 <<EOF
+${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1} $HEX_BOOTENV_SIZE
+${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2} $HEX_BOOTENV_SIZE
+EOF
+}
+
+# UBI specific version of the fw_env.config file.
+mender_create_fw_env_config_file_mender-ubi() {
+    # Takes one argument, which is the file to put it in.
+
+    set -x
+
+    # fw-utils seem to only be able to handle hex values.
+    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
+
+    # create fw_env.config file
+    cat > $1 <<EOF
+/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_1} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
+/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_2} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
+EOF
+}
+
+do_compile_append_mender-uboot() {
+    alignment_bytes=${MENDER_PARTITION_ALIGNMENT}
+    if [ $(expr ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET} % $alignment_bytes) -ne 0 ]; then
+        bberror "MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET must be aligned to" \
+                "MENDER_PARTITION_ALIGNMENT"
+    fi
+
+    if [ ! -e ${WORKDIR}/fw_env.config.default ]; then
+        mender_create_fw_env_config_file ${WORKDIR}/fw_env.config
+    else
+        cp ${WORKDIR}/fw_env.config.default ${WORKDIR}/fw_env.config
+    fi
+}
+
+do_install_append_mender-uboot() {
+    install -d -m 755 ${D}${sysconfdir}
+    ln -sf /data/u-boot/fw_env.config ${D}${sysconfdir}/fw_env.config
+
+    install -d ${D}/data/u-boot
+    install -m 0644 ${WORKDIR}/fw_env.config ${D}/data/u-boot/fw_env.config
+}

meta-mender-core/recipes-bsp/u-boot/u-boot-mender-helpers.inc
@@ -1,0 +1,42 @@
+BOOTENV_SIZE ??= "0x20000"
+BOOTENV_SIZE[doc] = "Size of bootloader environment"
+
+MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART ??= "0"
+
+################################################################################
+# Helpers and internal variables.
+################################################################################
+
+# @return the full path to the uboot environment device (IE /dev/mmcblkXbootY or /dev/mmcblkX)
+def mender_get_uboot_env_mmc_linux_device_path(d):
+    # Convert u-boot partition numbers to linux device references
+    # See [CONFIG_SYS_MMC_ENV_PART](https://github.com/u-boot/u-boot/blob/u-boot-2016.09.y/README#L4390) and
+    # [linux kernel mmc-dev-parts documentation](https://www.kernel.org/doc/Documentation/mmc/mmc-dev-parts.txt)
+    storage_device=d.getVar("MENDER_STORAGE_DEVICE",True)
+    env_part=d.getVar("MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART",True)
+    env_part_append=""
+    if env_part != "0":
+        env_part_append="boot%d" % (int(env_part)-1)
+    return storage_device + env_part_append
+
+MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH ?= "${@mender_get_uboot_env_mmc_linux_device_path(d)}"
+
+# This should evaluate to the same as MENDER_RESERVED_SPACE_BOOTLOADER_DATA.
+# The only reason it's not evaluated the same way is that we don't have the
+# necessary information (BOOTENV_SIZE) when evaluating
+# MENDER_RESERVED_SPACE_BOOTLOADER_DATA.  Note: If MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART is set to a non zero value
+# MENDER_RESERVED_SPACE_BOOTLOADER_DATA may be set to 0 to remove from rootfs size calculations.
+MENDER_BOOTENV_TOTAL_ALIGNED_SIZE = "${@mender_get_env_total_aligned_size(${BOOTENV_SIZE}, ${MENDER_PARTITION_ALIGNMENT})}"
+
+def mender_get_env_offset(start_offset, index, total_aligned_size):
+    if index == 1:
+        return "0x%x" % int(start_offset)
+    elif index == 2:
+        return "0x%x" % int(start_offset + total_aligned_size / 2)
+    else:
+        raise Exception("env index out of range in mender_get_env_offset: Should not happen")
+
+MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1 ?= "${@mender_get_env_offset(${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET}, 1, \
+                                                                      ${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE})}"
+MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2 ?= "${@mender_get_env_offset(${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET}, 2, \
+                                                                      ${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE})}"

meta-mender-core/recipes-bsp/u-boot/u-boot-mender.inc
@@ -2,35 +2,6 @@ require u-boot-mender-common.inc
 
 inherit deploy
 
-do_provide_mender_defines_append_mender-uboot() {
-    # Since the auto-provided fw-utils recipe will typically not have access to
-    # the proper BOOTENV_SIZE, let's create the fw_env.config file here, so that
-    # the mender_tar_src stage will provide it in its tar file.
-
-    if [ ! -e "${WORKDIR}/fw_env.config.default" ] && [ "${PREFERRED_PROVIDER_u-boot-fw-utils}" = "u-boot-fw-utils-mender-auto-provided" ]; then
-        mender_create_fw_env_config_file ${WORKDIR}/fw_env.config.default
-    fi
-}
-
-do_mender_tar_src() {
-    cd ${WORKDIR}
-    # Zip up all plain files, plus the source directory, but not the rest,
-    # because there are a lot of bitbake data directories.
-    rm -rf src-tar
-    cp -a ${S} src-tar
-    tar -cz --exclude-vcs -f ${TMPDIR}/mender-u-boot-src.tar.gz `find -maxdepth 1 -type f` src-tar
-}
-python() {
-    if not bb.utils.contains('DISTRO_FEATURES', 'mender-uboot', True, False, d):
-        return
-
-    if bb.utils.contains('MENDER_UBOOT_AUTO_CONFIGURE', '1', True, False, d):
-        aftertask = "do_mender_uboot_auto_configure"
-    else:
-        aftertask = "do_provide_mender_defines"
-    bb.build.addtask('do_mender_tar_src', 'do_configure', aftertask, d)
-}
-
 # Just to have a task if mender-uboot is not defined.
 do_deploy_append() {
     :

meta-mender-core/classes/mender-setup-uboot.inc
@@ -22,28 +22,3 @@ MENDER_RESERVED_SPACE_BOOTLOADER_DATA_DEFAULT_mender-uboot = \
 
 # make sure to provide a weak default
 UBOOT_SUFFIX ??= "bin"
-
-# Prefer to use our auto provided fw-utils variant which duplicates the sources
-# from u-boot, so we don't have to apply the same patches twice.
-PREFERRED_PROVIDER_u-boot-fw-utils ??= "${@mender_check_distro_features_early_contains(d, \
-                                                                                       'mender-uboot', \
-                                                                                       'u-boot-fw-utils-mender-auto-provided', \
-                                                                                       '')}"
-PREFERRED_RPROVIDER_u-boot-fw-utils ??= "${@mender_check_distro_features_early_contains(d, \
-                                                                                        'mender-uboot', \
-                                                                                        'u-boot-fw-utils-mender-auto-provided', \
-                                                                                        '')}"
-
-def mender_check_distro_features_early_contains(d, feature, if_true, if_false):
-    # Why this complicated function instead of checking DISTRO_FEATURES
-    # directly? It's because PREFERRED_PROVIDERs have some special early
-    # handling which happens before DISTRO_FEATURES is resolved, so we need to
-    # do this special early check.
-
-    in_df_backfill = bb.utils.contains('DISTRO_FEATURES_BACKFILL', feature, True, False, d)
-    in_df_backfill_considered = bb.utils.contains('DISTRO_FEATURES_BACKFILL_CONSIDERED', feature, True, False, d)
-
-    if in_df_backfill and not in_df_backfill_considered:
-        return if_true
-    else:
-        return if_false

meta-mender-core/recipes-bsp/u-boot/u-boot-fw-utils-mender-auto-provided_1.0.bb
@@ -1,78 +1,0 @@
-# Keep this separately from the rest of the .bb file in case that .bb file is
-# overridden from another layer.
-require u-boot-fw-utils-mender.inc
-
-
-
-#-------------------------------------------------------------------------------
-# Everything below here is dedicated to providing an automatic OOTB
-# u-boot-fw-utils recipe, for those configurations that lack it. It does this by
-# reusing the sources from the main U-Boot recipe itself.
-
-
-LICENSE = "GPL-2.0"
-# Specifying a dummy file here is slightly evil, but the problem is that since
-# this is a special recipe which uses source code coming from any odd U-Boot
-# version imaginable, it is not possible to use a stable checksum pointing to a
-# file from within that source. It is pretty unlikely that U-Boot will change
-# license, so use this for now.
-LIC_FILES_CHKSUM = "file://dummy-license.txt;md5=f02e326f800ee26f04df7961adbf7c0a"
-
-PROVIDES = "u-boot-fw-utils"
-RPROVIDES_${PN} = "u-boot-fw-utils"
-
-INSANE_SKIP_${PN} = "already-stripped"
-EXTRA_OEMAKE_class-target = 'CROSS_COMPILE=${TARGET_PREFIX} CC="${CC} ${CFLAGS} ${LDFLAGS}" HOSTCC="${BUILD_CC} ${BUILD_CFLAGS} ${BUILD_LDFLAGS}" V=1'
-DEPENDS += "bison-native"
-
-S = "${WORKDIR}/src-tar"
-
-def mender_preferred_uboot(d):
-    pref_uboot = d.getVar('PREFERRED_PROVIDER_u-boot')
-    if pref_uboot in [None, ""]:
-        pref_bootloader = d.getVar('PREFERRED_PROVIDER_virtual/bootloader')
-        if pref_bootloader and pref_bootloader.startswith("u-boot"):
-            return pref_bootloader
-        else:
-            return "u-boot"
-    else:
-        return pref_uboot
-MENDER_PREFERRED_UBOOT = "${@mender_preferred_uboot(d)}"
-
-
-do_patch() {
-    rm -rf ${S}
-    mkdir -p ${WORKDIR}
-    cd ${WORKDIR}
-    tar xzf ${TMPDIR}/mender-u-boot-src.tar.gz
-
-    # See LIC_FILES_CHKSUM.
-    echo dummy > ${S}/dummy-license.txt
-}
-do_patch[depends] += "${MENDER_PREFERRED_UBOOT}:do_mender_tar_src"
-
-do_compile () {
-    oe_runmake ${UBOOT_MACHINE}
-
-    # Detect what the build target to the environment tools is. It changed from
-    # "env" to "envtools" in v2017.09.
-    grep -q '^tools-all: *env\b' Makefile && ENV_TARGET=env
-    grep -q '^tools-all: *envtools\b' Makefile && ENV_TARGET=envtools
-    if [ -z "$ENV_TARGET" ]; then
-        echo "Could not determine environment tools target."
-        exit 1
-    fi
-
-    oe_runmake include/generated/timestamp_autogenerated.h include/generated/version_autogenerated.h || true
-    oe_runmake $ENV_TARGET
-}
-
-do_install () {
-    install -d ${D}${base_sbindir}
-    install -d ${D}${sysconfdir}
-    install -m 755 ${B}/tools/env/fw_printenv ${D}${base_sbindir}/fw_printenv
-    install -m 755 ${B}/tools/env/fw_printenv ${D}${base_sbindir}/fw_setenv
-    install -m 0644 ${B}/tools/env/fw_env.config ${D}${sysconfdir}/fw_env.config
-}
-
-PACKAGE_ARCH = "${MACHINE_ARCH}"

meta-mender-core/recipes-bsp/u-boot/u-boot-fw-utils-mender.inc
@@ -1,25 +1,0 @@
-require u-boot-mender-common.inc
-
-FILES_${PN}_append_mender-uboot = " /data/u-boot/fw_env.config"
-
-do_compile_append_mender-uboot() {
-    alignment_bytes=${MENDER_PARTITION_ALIGNMENT}
-    if [ $(expr ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET} % $alignment_bytes) -ne 0 ]; then
-        bberror "MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET must be aligned to" \
-                "MENDER_PARTITION_ALIGNMENT"
-    fi
-
-    if [ ! -e ${WORKDIR}/fw_env.config.default ]; then
-        mender_create_fw_env_config_file ${WORKDIR}/fw_env.config
-    else
-        cp ${WORKDIR}/fw_env.config.default ${WORKDIR}/fw_env.config
-    fi
-}
-
-do_install_append_mender-uboot() {
-    install -d -m 755 ${D}${sysconfdir}
-    ln -sf /data/u-boot/fw_env.config ${D}${sysconfdir}/fw_env.config
-
-    install -d ${D}/data/u-boot
-    install -m 0644 ${WORKDIR}/fw_env.config ${D}/data/u-boot/fw_env.config
-}

meta-mender-core/recipes-bsp/u-boot/u-boot-fw-utils_%.bbappend
@@ -1,3 +1,0 @@
-# Keep this separately from the rest of the .bb file in case that .bb file is
-# overridden from another layer.
-require u-boot-fw-utils-mender.inc

meta-mender-core/recipes-bsp/u-boot/u-boot-mender-common.inc
@@ -1,15 +1,12 @@
 inherit uboot-config
 
-DEPENDS += "coreutils-native"
+include u-boot-mender-helpers.inc
 
-BOOTENV_SIZE ??= "0x20000"
-BOOTENV_SIZE[doc] = "Size of bootloader environment"
+DEPENDS += "coreutils-native"
 
 DEPLOYDIR = "${WORKDIR}/deploy-${PN}"
 
 MENDER_UBOOT_AUTO_CONFIGURE ??= "${@bb.utils.contains('DISTRO_FEATURES', 'mender-uboot', '1', '0', d)}"
-
-MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART ??= "0"
 
 ################################################################################
 # Patches.
@@ -32,12 +29,6 @@ SRC_URI_append_mender-uboot = "${@bb.utils.contains('MENDER_UBOOT_AUTO_CONFIGURE
 ################################################################################
 do_provide_mender_defines() {
     set -x
-
-    if echo "${PN}" | fgrep "mender-auto-provided"; then
-        # "mender-auto-provided" is a special recipe that has its sources
-        # copied from the original U-Boot, so this step has already been done.
-        exit 0
-    fi
 
     if ! ${@bb.utils.contains('DISTRO_FEATURES', 'mender-uboot', 'true', 'false', d)}; then
         bbfatal "To compile U-Boot with mender you have to add 'mender-uboot' to MENDER_FEATURES_ENABLE or DISTRO_FEATURES."
@@ -299,12 +290,6 @@ MENDER_UBOOT_TMP_SRC = "${WORKDIR}/tmp-src"
 MENDER_UBOOT_OLD_SRC = "${WORKDIR}/old-src"
 
 do_mender_uboot_auto_configure() {
-    if echo "${PN}" | fgrep "mender-auto-provided"; then
-        # "mender-auto-provided" is a special recipe that has its sources
-        # copied from the original U-Boot, so this step has already been done.
-        exit 0
-    fi
-
     if [ -n "${EXTERNALSRC}" ]; then
         bbwarn "Skipping U-Boot auto-configuration when EXTERNALSRC is set. Would change the source files. If you need the patch, turn off EXTERNALSRC, call 'bitbake -c save_mender_auto_configured_patch u-boot' to generate a patch, and then apply that to your work tree and reenable EXTERNALSRC."
         exit 0
@@ -372,75 +357,6 @@ python() {
         bb.build.addtask('do_mender_uboot_auto_configure', 'do_configure', 'do_provide_mender_defines', d)
         bb.build.addtask('do_save_mender_auto_configured_patch', '', 'do_mender_uboot_auto_configure', d)
 }
-
-################################################################################
-# Helpers and internal variables.
-################################################################################
-
-# @return the full path to the uboot environment device (IE /dev/mmcblkXbootY or /dev/mmcblkX)
-def mender_get_uboot_env_mmc_linux_device_path(d):
-    # Convert u-boot partition numbers to linux device references
-    # See [CONFIG_SYS_MMC_ENV_PART](https://github.com/u-boot/u-boot/blob/u-boot-2016.09.y/README#L4390) and
-    # [linux kernel mmc-dev-parts documentation](https://www.kernel.org/doc/Documentation/mmc/mmc-dev-parts.txt)
-    storage_device=d.getVar("MENDER_STORAGE_DEVICE",True)
-    env_part=d.getVar("MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART",True)
-    env_part_append=""
-    if env_part != "0":
-        env_part_append="boot%d" % (int(env_part)-1)
-    return storage_device + env_part_append
-
-MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH ?= "${@mender_get_uboot_env_mmc_linux_device_path(d)}"
-
-mender_create_fw_env_config_file() {
-    # Takes one argument, which is the file to put it in.
-
-    set -x
-
-    # fw-utils seem to only be able to handle hex values.
-    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
-
-    # create fw_env.config file
-    cat > $1 <<EOF
-${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1} $HEX_BOOTENV_SIZE
-${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2} $HEX_BOOTENV_SIZE
-EOF
-}
-
-# UBI specific version of the fw_env.config file.
-mender_create_fw_env_config_file_mender-ubi() {
-    # Takes one argument, which is the file to put it in.
-
-    set -x
-
-    # fw-utils seem to only be able to handle hex values.
-    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
-
-    # create fw_env.config file
-    cat > $1 <<EOF
-/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_1} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
-/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_2} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
-EOF
-}
-
-# This should evaluate to the same as MENDER_RESERVED_SPACE_BOOTLOADER_DATA.
-# The only reason it's not evaluated the same way is that we don't have the
-# necessary information (BOOTENV_SIZE) when evaluating
-# MENDER_RESERVED_SPACE_BOOTLOADER_DATA.  Note: If MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART is set to a non zero value
-# MENDER_RESERVED_SPACE_BOOTLOADER_DATA may be set to 0 to remove from rootfs size calculations.
-MENDER_BOOTENV_TOTAL_ALIGNED_SIZE = "${@mender_get_env_total_aligned_size(${BOOTENV_SIZE}, ${MENDER_PARTITION_ALIGNMENT})}"
-
-def mender_get_env_offset(start_offset, index, total_aligned_size):
-    if index == 1:
-        return "0x%x" % int(start_offset)
-    elif index == 2:
-        return "0x%x" % int(start_offset + total_aligned_size / 2)
-    else:
-        raise Exception("env index out of range in mender_get_env_offset: Should not happen")
-
-MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1 ?= "${@mender_get_env_offset(${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET}, 1, \
-                                                                      ${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE})}"
-MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2 ?= "${@mender_get_env_offset(${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET}, 2, \
-                                                                      ${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE})}"
 
 # Ignore this, only used for testing.
 # The reason it's here is so that the test URI is appended last.

commit 9767cb3671eb50b6ef184cbddf09f1bf7c651a95

MEN-3834: Fix incorrect BOOTENV_SIZE value being used in libubootenv.
Changelog: Fix incorrect BOOTENV_SIZE value being used in libubootenv
recipe. The symptom of this was a non-working set of `fw_printenv` and
`fw_setenv` tools:
```
root@raspberrypi4:~# fw_printenv
Cannot read environment, using default
Cannot read default environment from file
```

This was accidentally broken in commit f61fa534d43dcd, where the
task was originally handled by the u-boot-fw-utils recipe files.

Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

meta-mender-core/recipes-bsp/u-boot/libubootenv_%.bbappend
@@ -2,37 +2,6 @@ include u-boot-mender-helpers.inc
 
 FILES_${PN}_append_mender-uboot = " /data/u-boot/fw_env.config"
 
-mender_create_fw_env_config_file() {
-    # Takes one argument, which is the file to put it in.
-
-    set -x
-
-    # fw-utils seem to only be able to handle hex values.
-    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
-
-    # create fw_env.config file
-    cat > $1 <<EOF
-${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1} $HEX_BOOTENV_SIZE
-${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2} $HEX_BOOTENV_SIZE
-EOF
-}
-
-# UBI specific version of the fw_env.config file.
-mender_create_fw_env_config_file_mender-ubi() {
-    # Takes one argument, which is the file to put it in.
-
-    set -x
-
-    # fw-utils seem to only be able to handle hex values.
-    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
-
-    # create fw_env.config file
-    cat > $1 <<EOF
-/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_1} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
-/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_2} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
-EOF
-}
-
 do_compile_append_mender-uboot() {
     alignment_bytes=${MENDER_PARTITION_ALIGNMENT}
     if [ $(expr ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET} % $alignment_bytes) -ne 0 ]; then
@@ -40,12 +9,13 @@ do_compile_append_mender-uboot() {
                 "MENDER_PARTITION_ALIGNMENT"
     fi
 
-    if [ ! -e ${WORKDIR}/fw_env.config.default ]; then
+    if [ ! -e ${DEPLOY_DIR_IMAGE}/fw_env.config.default ]; then
         mender_create_fw_env_config_file ${WORKDIR}/fw_env.config
     else
-        cp ${WORKDIR}/fw_env.config.default ${WORKDIR}/fw_env.config
+        cp ${DEPLOY_DIR_IMAGE}/fw_env.config.default ${WORKDIR}/fw_env.config
     fi
 }
+do_compile[depends] += "u-boot:do_deploy"
 
 do_install_append_mender-uboot() {
     install -d -m 755 ${D}${sysconfdir}

meta-mender-core/recipes-bsp/u-boot/u-boot-mender-helpers.inc
@@ -40,3 +40,34 @@ MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1 ?= "${@mender_get_env_offset(${MENDER_U
                                                                       ${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE})}"
 MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2 ?= "${@mender_get_env_offset(${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET}, 2, \
                                                                       ${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE})}"
+
+mender_create_fw_env_config_file() {
+    # Takes one argument, which is the file to put it in.
+
+    set -x
+
+    # fw-utils seem to only be able to handle hex values.
+    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
+
+    # create fw_env.config file
+    cat > $1 <<EOF
+${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1} $HEX_BOOTENV_SIZE
+${MENDER_UBOOT_MMC_ENV_LINUX_DEVICE_PATH} ${MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2} $HEX_BOOTENV_SIZE
+EOF
+}
+
+# UBI specific version of the fw_env.config file.
+mender_create_fw_env_config_file_mender-ubi() {
+    # Takes one argument, which is the file to put it in.
+
+    set -x
+
+    # fw-utils seem to only be able to handle hex values.
+    HEX_BOOTENV_SIZE="$(printf 0x%x "${BOOTENV_SIZE}")"
+
+    # create fw_env.config file
+    cat > $1 <<EOF
+/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_1} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
+/dev/${MENDER_STORAGE_DEVICE}_${MENDER_UBOOT_ENV_UBIVOL_NUMBER_2} 0 $HEX_BOOTENV_SIZE ${MENDER_UBI_LEB_SIZE}
+EOF
+}

meta-mender-core/recipes-bsp/u-boot/u-boot-mender.inc
@@ -35,6 +35,13 @@ do_provide_mender_defines() {
         bbfatal "To compile U-Boot with mender you have to add 'mender-uboot' to MENDER_FEATURES_ENABLE or DISTRO_FEATURES."
     fi
 
+    # Since the libubootenv recipe will typically not have access to the proper
+    # BOOTENV_SIZE, let's create the fw_env.config file here, so that it can
+    # pick it from the deploy directory.
+    if [ ! -e "${WORKDIR}/fw_env.config.default" ]; then
+        mender_create_fw_env_config_file ${WORKDIR}/fw_env.config.default
+    fi
+
     if [ ${MENDER_UBOOT_CONFIG_SYS_MMC_ENV_PART} -eq "0" ]; then
         # Only check reserved space for bootloader data when the bootloader data is saved to the user partition of the MMC.
         # In cases where it's saved to the boot partition it's not a part of the rootfs size and MENDER_RESERVED_SPACE_BOOTLOADER_DATA
@@ -380,6 +387,8 @@ do_deploy_append_mender-uboot() {
     # file.
     dd if=/dev/zero of=${WORKDIR}/uboot.env bs=${MENDER_BOOTENV_TOTAL_ALIGNED_SIZE} count=1
     install -m 644 ${WORKDIR}/uboot.env ${DEPLOYDIR}/uboot.env
+
+    install -m 644 ${WORKDIR}/fw_env.config.default ${DEPLOYDIR}/fw_env.config.default
 }
 addtask deploy after do_compile before do_package
 