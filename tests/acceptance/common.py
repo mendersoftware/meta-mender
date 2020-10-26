7d6a86c946c6f74a94ff544da21d3b20d17820ce
Lluis Campos <lluis.campos@northern.tech>
2020-10-26 15:37:30 +0100

    [tests/acceptance] common.py: Remove try/catch in put_no_sftp
    It added no value to the function. Also now both put/get functions are
    aligned.
    
    Changelog: None
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/acceptance/common.py
+++ tests/acceptance/common.py
@@ -236,15 +236,9 @@ def get_ssh_common_args(conn):
 # Yocto build SSH is lacking SFTP, let's override and use regular SCP instead.
 def put_no_sftp(file, conn, remote="."):
     cmd = "scp %s" % get_ssh_common_args(conn)
-
-    try:
-        conn.local(
-            "%s -P %s %s %s@%s:%s"
-            % (cmd, conn.port, file, conn.user, conn.host, remote)
-        )
-    except Exception as e:
-        print("exception while putting file: ", e)
-        raise e
+    conn.local(
+        "%s -P %s %s %s@%s:%s" % (cmd, conn.port, file, conn.user, conn.host, remote)
+    )
 
 
 # Yocto build SSH is lacking SFTP, let's override and use regular SCP instead.
