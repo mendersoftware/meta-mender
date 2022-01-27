41781ce071ed5fb9ceb9312ad4383299ba1ac68c
Ole Petter <ole.orhagen@northern.tech>
2022-01-27 18:48:45 +0100

    MEN-5300: add a recipe for mender-gateway
    Adds a closed source Yocto recipe for mender-gateway. Only the git
    recipe is included in this commit. The recipe takes a tarball containing
    the pre-compiled binaries and support files and just installs them on
    the target.
    
    A piece of code from mender-monitor has been refactored into a common
    class for reuse in all closed source recipes that define SRCREV from the
    tarball filename.
    
    Acceptance test added accordingly. And default storage size increased to
    fit mender-gateway for the test.
    
    Changelog: Add recipe for mender-gateway_git
    Changelog: Add testing image mender-gateway-image-full-cmdline
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

---
+++ tests/acceptance/commercial/test_mender_gateway.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# Copyright 2022 Northern.tech AS
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import subprocess
+
+import pytest
+
+from utils.common import (
+    build_image,
+    latest_build_artifact,
+)
+
+
+@pytest.mark.commercial
+@pytest.mark.min_mender_version("3.3.0")
+class TestMenderGateway:
+    @pytest.mark.only_with_image("ext4")
+    def test_build_mender_gateway(
+        self, request, bitbake_variables, prepared_test_build, bitbake_image
+    ):
+        build_image(
+            prepared_test_build["build_dir"],
+            prepared_test_build["bitbake_corebase"],
+            bitbake_image,
+            ['IMAGE_INSTALL_append = " mender-gateway"'],
+            [
+                'BBLAYERS_append = " %s/../meta-mender-commercial"'
+                % bitbake_variables["LAYERDIR_MENDER"]
+            ],
+        )
+        image = latest_build_artifact(
+            request, prepared_test_build["build_dir"], "core-image*.ext4"
+        )
+
+        output = subprocess.check_output(
+            ["debugfs", "-R", "stat /usr/bin/mender-gateway", image]
+        ).decode()
+        assert "Type: regular" in output
