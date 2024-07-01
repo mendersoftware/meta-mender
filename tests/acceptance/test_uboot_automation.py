2cd3b718e28f39c4d43352aac76a624f55b259ae
Lluis Campos <lluis.campos@northern.tech>
2024-07-01 19:32:06 +0200

    test: Upate fail ratio for test_uboot_compile on flash QEMU
    The failed ratio has increased in a similar way to vxpress board. See
    related commit 299349d769b8f81f8db3d88f3cc5fdd6162832bb.
    
    Ticket: MEN-7298
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/acceptance/test_uboot_automation.py
+++ tests/acceptance/test_uboot_automation.py
@@ -352,8 +352,8 @@ class TestUbootAutomation:
                 measured_failed_ratio = 66.0 / 535.0
             elif machine == "vexpress-qemu-flash":
                 # PLEASE UPDATE the version you used to find this number if you update it.
-                # From version: 2022.01-gitd637294e264a
-                measured_failed_ratio = 9.0 / 130.0
+                # From version: 2024.01
+                measured_failed_ratio = 19.0 / 122.0
 
             # We tolerate a certain percentage discrepancy in either direction.
             tolerated_discrepancy = 0.1
