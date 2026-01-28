0f5535284ca40a3b46300414322703fee9e89a26
Lluis Campos <lluis.campos@northern.tech>
2026-01-28 13:01:34 +0100

    test: Move parsing logic from subprocess to native Python
    This is a workaround for a elusive race condition in our test suite.
    There was a previous attempt at:
    * https://github.com/mendersoftware/meta-mender/pull/2382
    
    Which did not solve the issue when combined with code that actually
    removed tests (and reordered the rest):
    * Pull Request: https://github.com/mendersoftware/meta-mender/pull/2375
    * Example failure: https://gitlab.com/Northern.tech/Mender/mender-qa/-/jobs/12712421268
    
    Although I cannot explain exactly why, moving the logic to native Python
    seems to fix the problem. See this green run where `test_uboot_compile`
    indeed starts before the other worker has finished and still succeeds.
    It is from the previously referenced PR with a wip version of this very
    commit. Link:
    * https://gitlab.com/Northern.tech/Mender/mender-qa/-/jobs/12893103511
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>
    (cherry picked from commit f250c43d8d48d81422f2a3dca7ee530bee19b5de)

--- tests/acceptance/test_uboot_automation.py
+++ tests/acceptance/test_uboot_automation.py
@@ -86,15 +86,21 @@ class TestUbootAutomation:
         days_to_be_old = 7
 
         # Find the repository directories we need
-        [poky_dir, meta_mender_dir, _] = (
-            subprocess.check_output(
-                "bitbake-layers show-layers | awk '$1~/(^core$|^mender$)/ {print $2}' | xargs -n 1 dirname",
-                cwd=os.environ["BUILDDIR"],
-                shell=True,
-            )
-            .decode()
-            .split("\n")
-        )
+        poky_dir = None
+        meta_mender_dir = None
+        bitbake_layers_output = subprocess.check_output(
+            "bitbake-layers show-layers", cwd=os.environ["BUILDDIR"], shell=True,
+        ).decode()
+        for line in bitbake_layers_output.split("\n"):
+            fields = line.split()
+            if len(fields) >= 2:
+                layer_name = fields[0]
+                layer_path = fields[1]
+
+                if layer_name == "core":
+                    poky_dir = os.path.dirname(layer_path)
+                elif layer_name == "mender":
+                    meta_mender_dir = os.path.dirname(layer_path)
 
         # SHA from poky repository, limited by date.
         poky_rev = (
